046e0b7cdf6e62fd9d7495f95790d0c77541aec1
Olivier Henley <olivier.henley@gmail.com>
2025-04-17 20:25:46 -0400

    being improvement on serialization speed

--- src/exporter/del-export.adb
+++ src/exporter/del-export.adb
@@ -51,7 +51,8 @@ package body Del.Export is
       return R;
    end Get_Max;
 
-   procedure Generate_Grid(Self : in Del.Model.Model; Out_S : in out Unbounded_String) is
+   function Generate_Grid (Self : in Del.Model.Model) return Tensor_T is
+   --procedure Generate_Grid(Self : in Del.Model.Model; Out_S : in out Unbounded_String) is
       -- pull in the dataset
       Dataset     : constant Training_Data_Access := Del.Model.Get_Dataset(Self);
       Data_Tensor : constant Tensor_T := Dataset.Get_Data;
@@ -70,6 +71,8 @@ package body Del.Export is
 
       -- batch buffer
       Grid_Input : Tensor_T := Zeros((Num_X * Num_Y, 2));
+      Grid_Data  : Tensor_T := Zeros((Num_X * Num_Y, 3));
+
       Num_Classes : constant Positive := Shape(Self.Run_Layers(Zeros((1,2))))(2);
       All_Preds : Tensor_T := Zeros((Num_X * Num_Y, Num_Classes));
       Pred_Counter : array (1 .. Num_Classes) of Natural := (others => 0);
@@ -79,6 +82,8 @@ package body Del.Export is
       begin
          return (I-1) * Num_Y + J;
       end Index;
+
+      First : Boolean := True;
 
    begin
       -- fill entire grid in one shot
@@ -92,16 +97,10 @@ package body Del.Export is
       -- single batch prediction
       All_Preds := Self.Run_Layers(Grid_Input);
 
-      -- begin JSON
-      Out_S := Out_S & To_Unbounded_String("    ""grid"": [") & ASCII.LF;
-
       -- unpack every row
-      declare
-         First : Boolean := True;
-      begin
          for I in 1 .. Num_X loop
             for J in 1 .. Num_Y loop
-               declare
+            declare
                Pred_Index : Integer   := 1;
                Max_Val    : Float_32 := All_Preds.Get((Index(I, J), 1));
             begin
@@ -115,35 +114,32 @@ package body Del.Export is
 
                Pred_Counter(Pred_Index) := Pred_Counter(Pred_Index) + 1;
 
-               -- emit JSON entry
-               if not First then
-                  Out_S := Out_S & To_Unbounded_String(",");
-               end if;
-               Out_S := Out_S
-                     & ASCII.LF
-                     & To_Unbounded_String("        [")
-                     & To_Unbounded_String(Trim(Float_32'Image(X_Min + Float_32(I-1)*Step), Left))
-                     & To_Unbounded_String(", ")
-                     & To_Unbounded_String(Trim(Float_32'Image(Y_Min + Float_32(J-1)*Step), Left))
-                     & To_Unbounded_String(", ")
-                     & To_Unbounded_String(Integer'Image(Pred_Index))
-                     & To_Unbounded_String("]");
-               First := False;
+               declare
+                  X : Float_32 := X_Min + Float_32 (I-1)*Step;
+                  Y : Float_32 := Y_Min + Float_32 (J-1)*Step;
+                  Class : Float_32 := Float_32 (Pred_Index);
+               begin
+                  Grid_Data.Set((Index(I, J), 1), X);
+                  Grid_Data.Set((Index(I, J), 2), Y);
+                  Grid_Data.Set((Index(I, J), 3), Class);
+               end;
+
             end;
             end loop;
          end loop;
-      end;
-
-      Out_S := Out_S & ASCII.LF & To_Unbounded_String("    ]");
 
       -- print to see if any class is zero
       for K in Pred_Counter'Range loop
          Put_Line("  Grid hit count for class " & Integer'Image(K)
                   & ": " & Natural'Image(Pred_Counter(K)));
       end loop;
+
+      return Grid_Data;
    end Generate_Grid;
 
 
+   function Grid_Line (X : Float_32; Y : Float_32; Class_Nbr : Float_32) return String is
+      ("[" &  X'Img & ", " &  Y'Img & ", " &  Class_Nbr'Img  & "]");
 
 begin
    Create(File, Out_File, Filename);
@@ -243,15 +239,32 @@ begin
       end if;
 
       if Include_Grid then
-         JSON_Content := JSON_Content & "," & New_Line_Str;
-         Generate_Grid(Self, Grid_Export); 
-         JSON_Content := JSON_Content & Grid_Export;
+         JSON_Content := JSON_Content & ",";
+         Put(File, To_String(JSON_Content));
+         New_Line (File);
+         Put_Line (File, "    ""grid"": [");
+         declare
+            function Generate_G(Self : in Del.Model.Model) return Tensor_T is
+            begin
+               return Zeros((1, 3));
+            end Generate_G;
+            Grid_Data : Tensor_T := Generate_Grid (Self);
+            S : Tensor_Shape := Shape (Grid_Data);
+         begin
+            Put_Line (S'Image);
+            for I in 1 .. S (1) loop
+               Put (File, Grid_Line (Grid_Data.Get((I, 1)),
+                                     Grid_Data.Get((I, 2)),
+                                     Grid_Data.Get((I, 3))));
+               if I < S (1) then
+                  Put_Line (File, ",");
+               end if;   
+            end loop;
+         end;
+            Put (File, "]");
       end if;
 
-      JSON_Content := JSON_Content & New_Line_Str & "}";
-
-      -- Write to file
-      Put(File, To_String(JSON_Content));
+      Put_Line (File, "}");
       Close(File);
 
       Put_Line("Model and grid exported successfully to " & Filename);
